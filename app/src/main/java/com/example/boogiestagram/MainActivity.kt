b04bc14205eabef24228f9d4fe5c8abb6b628218
KimSo-jung <mobssy21@gmail.com>
2022-11-27 23:56:28 +0900

    login change?

--- app/src/main/java/com/example/boogiestagram/MainActivity.kt
+++ app/src/main/java/com/example/boogiestagram/MainActivity.kt
@@ -51,11 +51,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
                 return true
             }
+            /*
             R.id.action_search -> {
                 var gridFragment = GridFragment()
                 supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
                 return true
             }
+             */
             R.id.action_add_photo -> {
                 if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
                     startActivity(Intent(this, AddPhotoActivity::class.java))
@@ -67,11 +69,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
                 return true
             }
+            /*
             R.id.action_account -> {
                 var userFragment = UserFragment()
                 supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
                 return true
             }
+             */
         }
         return false
     }
